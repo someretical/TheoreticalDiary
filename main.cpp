1a2bc9f4341447df9ecf796c7a01d5a45769c619
someretical <29365738+someretical@users.noreply.github.com>
2021-09-30 23:41:58 +1000

    added clang formatting
    - set the default keys for all buttons

--- main.cpp
+++ main.cpp
@@ -2,14 +2,13 @@
 
 #include <QApplication>
 
-int main(int argc, char* argv[])
-{
-    // This is to fix fonts not scaling properly at different DPI
-    // https://stackoverflow.com/a/36058882
-    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+int main(int argc, char *argv[]) {
+  // This is to fix fonts not scaling properly at different DPI
+  // https://stackoverflow.com/a/36058882
+  QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+  QApplication a(argc, argv);
+  MainWindow w;
+  w.show();
+  return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,66 +2,55 @@
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    : QMainWindow(parent), ui(new Ui::MainWindow)
+
 {
-    ui->setupUi(this);
+  ui->setupUi(this);
 
-    // Setup normal buttons
-    connect(ui->open_button, SIGNAL(clicked()), this, SLOT(open_button_pressed()));
-    connect(ui->new_button, SIGNAL(clicked()), this, SLOT(new_button_pressed()));
-    connect(ui->dl_button, SIGNAL(clicked()), this, SLOT(dl_button_pressed()));
-    connect(ui->import_button, SIGNAL(clicked()), this, SLOT(import_button_pressed()));
-    connect(ui->flush_button, SIGNAL(clicked()), this, SLOT(flush_button_pressed()));
-    connect(ui->dump_button, SIGNAL(clicked()), this, SLOT(dump_button_pressed()));
+  // Setup normal buttons
+  connect(ui->open_button, SIGNAL(clicked()), this,
+          SLOT(open_button_pressed()));
+  connect(ui->new_button, SIGNAL(clicked()), this, SLOT(new_button_pressed()));
+  connect(ui->dl_button, SIGNAL(clicked()), this, SLOT(dl_button_pressed()));
+  connect(ui->import_button, SIGNAL(clicked()), this,
+          SLOT(import_button_pressed()));
+  connect(ui->flush_button, SIGNAL(clicked()), this,
+          SLOT(flush_button_pressed()));
+  connect(ui->dump_button, SIGNAL(clicked()), this,
+          SLOT(dump_button_pressed()));
 
-    // Setup expandable options
-    ui->options->hide();
-    connect(ui->options_button, SIGNAL(clicked()), this, SLOT(toggle_advanced_options()));
+  // Setup expandable options
+  ui->options->hide();
+  connect(ui->options_button, SIGNAL(clicked()), this,
+          SLOT(toggle_advanced_options()));
 
-    // Setup quit action
-    auto action_quit = this->findChild<QAction *>("action_quit");
-    addAction(action_quit);
-    connect(action_quit, SIGNAL(triggered()), this, SLOT(quit_app()), Qt::QueuedConnection);
+  // Setup quit action
+  auto action_quit = this->findChild<QAction *>("action_quit");
+  addAction(action_quit);
+  connect(action_quit, SIGNAL(triggered()), this, SLOT(quit_app()),
+          Qt::QueuedConnection);
 }
 
-MainWindow::~MainWindow()
+MainWindow::~MainWindow() { delete ui; }
+
+void MainWindow::open_button_pressed() {}
+
+void MainWindow::new_button_pressed() {}
+
+void MainWindow::dl_button_pressed() {}
+
+void MainWindow::import_button_pressed() {}
+
+void MainWindow::flush_button_pressed() {}
+
+void MainWindow::dump_button_pressed() {}
+
+void MainWindow::toggle_advanced_options()
 {
-    delete ui;
-}
-
-void MainWindow::open_button_pressed()
-{
-
-}
-
-void MainWindow::new_button_pressed() {
-
-}
-
-void MainWindow::dl_button_pressed(){
-
-}
-
-void MainWindow::import_button_pressed(){
-
-}
-
-void MainWindow::flush_button_pressed(){
-
-}
-
-void MainWindow::dump_button_pressed(){
-
-}
-
-void MainWindow::toggle_advanced_options() {
     if (ui->options->isVisible())
         ui->options->hide();
-    else ui->options->show();
+    else
+        ui->options->show();
 }
 
-void MainWindow::quit_app() {
-    qApp->quit();
-}
-
+void MainWindow::quit_app() { qApp->quit(); }
